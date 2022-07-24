45573610d70bde7ce4f4dfd7ba05def4bf93cd6d
Viral Mehta <virallmehta@gmail.com>
2022-07-24 19:37:13 +0530

    added WordPres Coding Standard with composer

--- wordpress/wp-content/plugins/workflowtest/workflowtest.php
+++ wordpress/wp-content/plugins/workflowtest/workflowtest.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * @package Workflowtest
+ * @version 1.0.0
+ */
+
 /*
 Plugin Name: WorkFlow Test
 Plugin URI: http://wplab.test/
-Description: WorkFlowTest
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Viral Mehta
 Version: 1.0.0
 Author URI: http://wplab.test/
+Summary: Define the WordPress-specific global variables.
 */
