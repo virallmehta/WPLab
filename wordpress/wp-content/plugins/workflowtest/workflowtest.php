eeba5a449fee762a4c698357644ea1389a9827b1
Viral Mehta <virallmehta@gmail.com>
2022-07-25 09:24:24 +0530

    added WordPres Coding Standard with composer

--- wordpress/wp-content/plugins/workflowtest/workflowtest.php
+++ wordpress/wp-content/plugins/workflowtest/workflowtest.php
@@ -1,8 +1,17 @@
 <?php
-/**
- * @package Workflowtest
- * @version 1.0.0
- */
+  /**
+   * Footer
+   *
+   * Main footer file for the theme.
+   *
+   * @category   Components
+   * @package    WordPress
+   * @subpackage Theme_Name_Here
+   * @author     Your Name <yourname@example.com>
+   * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+   * @link       https://yoursite.com
+   * @since      1.0.0
+   */
 
 /*
 Plugin Name: WorkFlow Test
